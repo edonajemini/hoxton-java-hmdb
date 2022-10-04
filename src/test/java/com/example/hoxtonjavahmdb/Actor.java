ab0a76d52b0c5d336f2257cb233722bb7d66cdba
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-10-04 18:13:43 +0200

    Updates

---
+++ src/test/java/com/example/hoxtonjavahmdb/Actor.java
@@ -0,0 +1,13 @@
+package com.example.hoxtonjavahmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actor {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/example/hoxtonjavahmdb/Movie.java
@@ -0,0 +1,13 @@
+package com.example.hoxtonjavahmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Movie {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
